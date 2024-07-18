9ec37991f9576ad3821d2ad654ba50654d422fda
Evil Angel <angel@nocrynomercy.ru>
2024-07-18 17:34:34 +0300

    domain acception

--- app.js
+++ app.js
@@ -7,6 +7,25 @@ app.use(express.static('public', {
   maxAge: '30d'
 }));
 
+// Middleware для проверки доменного имени
+const allowDomainMiddleware = (req, res, next) => {
+  const allowedHost = 'www.nocrynomercy.ru'; // Замените на ваш домен
+
+  // Получаем хост из заголовка запроса
+  const host = req.headers.host;
+
+  // Проверяем, соответствует ли хост допустимому домену
+  if (host !== allowedHost) {
+    return res.status(403).send('');
+  }
+
+  // Если хост соответствует, продолжаем выполнение запроса
+  next();
+};
+
+// Использование middleware для всех маршрутов
+app.use(allowDomainMiddleware);
+
 app.get('/', (req, res) => {
     res.render('index');
 });
