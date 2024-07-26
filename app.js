04241ff16606e6a87245537fe083b3fc235a0c00
Evil Angel <angel@nocrynomercy.ru>
2024-07-26 12:46:23 +0300

    app js

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+const ensureSecure = (req, res, next) => {
+  // Проверяем, если запрос не безопасен (не HTTPS)
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    // Перенаправляем на тот же URL, но с HTTPS
+    return res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+  next();
+};
+
+app.enable('trust proxy');
+
+app.use(ensureSecure);
+
 app.set('view engine', 'ejs');
 app.use(express.static('public', {
   maxAge: '30d'
@@ -12,7 +25,7 @@ const allowDomainMiddleware = (req, res, next) => {
 
   const host = req.headers.host;
   if (!allowedHosts.includes(host)) {
-    return res.status(403).send('');
+    return res.status(403).send('Access forbidden');
   }
 
   next();
